4c211ddabfd7338ebfc40c01ecc08e16e3e2f228
martinrebo <martinrg33@gmail.com>
2020-08-18 14:55:26 +0200

    :tada: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+import HeaderTitle from './components/Header/HeaderTitle'
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderTitle />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/HeaderTitle.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HeaderTitle from "./HeaderTitle";
+
+
+test('Header contains html <header> tag', () => {
+
+  });
+  
