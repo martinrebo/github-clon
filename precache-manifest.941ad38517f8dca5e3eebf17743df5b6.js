3c25a1514dc561da36917620360cfb150fb62edd
martinrebo <martinrg33@gmail.com>
2020-08-25 12:04:12 +0200

    Updates

---
+++ precache-manifest.941ad38517f8dca5e3eebf17743df5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1db2fcebc8c306adfcbe458cd7efc3",
+    "url": "/github-clon/index.html"
+  },
+  {
+    "revision": "987dcb735447d727288a",
+    "url": "/github-clon/static/css/main.f0c9e138.chunk.css"
+  },
+  {
+    "revision": "11e17cbd6406522140f3",
+    "url": "/github-clon/static/js/2.ec1d660f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-clon/static/js/2.ec1d660f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987dcb735447d727288a",
+    "url": "/github-clon/static/js/main.cd73ffd3.chunk.js"
+  },
+  {
+    "revision": "f51cd48e9a4c524fe7dc",
+    "url": "/github-clon/static/js/runtime-main.368e80bd.js"
+  }
+]);
